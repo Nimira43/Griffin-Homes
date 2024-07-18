1087bcf2c004695ebab3e3789dc3dc1ac215e04f
Nick Rabone <n.rabone72@aol.com>
2024-07-18 23:50:36 +0100

    Continue to work on navbar and brought in logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ const App = () => {
       <nav className="bg-[#111] border-b border-[#ff4500]">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
-            
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+              <a href="/index.html" className="flex flex-shrink-0 items-center mr-4">
+                <img src="../images/logo.png" alt="Jobsearch" className="h-10 w-auto" />
+                <span className="hidden md:block text-2xl font-bold ml-2 text-[#ff4500]">JobSearch</span>
+              </a>
+            </div>
           </div>
         </div>
 
