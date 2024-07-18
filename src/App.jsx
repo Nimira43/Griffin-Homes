b05c2b7208697fbdfc61a277fcfdff9e02001d8f
Nick Rabone <n.rabone72@aol.com>
2024-07-18 23:17:49 +0100

    Starting work on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 const App = () => {
   return (
-    <div>
-      <h1>Jobsearch</h1>      
-    </div>
+    <>
+      <nav className="bg-[#111] border-b border-[#ff4500]">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            
+          </div>
+        </div>
+
+      </nav>
+      
+      <section>
+
+      </section>
+
+      <section>
+
+      </section>
+
+      <section>
+
+      </section>
+
+      <section>
+
+      </section>
+
+    </>
   )
 }
 
