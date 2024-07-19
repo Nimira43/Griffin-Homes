c3cb8076a9441cd48e859a79d7b2550f74b6be39
Nick Rabone <n.rabone72@aol.com>
2024-07-19 02:04:36 +0100

    Move some code into new Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 const App = () => {
   return (
     <>
       <Navbar />
-      
-      
-      <section className="py-20 mb-4">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-          <div className="text-center">
-            <h1 className="text-4xl text-[#111] sm:text-5xl md:text-6xl">
-              Find the Right Coding Job
-            </h1>
-            <p className="my-4 text-xl text-[#ff4500]">
-              Discover the many opportunities waiting for you. 
-            </p>
-          </div>
-        </div>
-      </section>
-
+      <Hero />
       <section className="py-4">
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
