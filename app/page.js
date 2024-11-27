85c023f9f3ed8f275e0a83690cc5e15d10e03ac0
Nimira43 <n.rabone72@aol.com>
2024-11-27 21:52:25 +0000

    Added params to Property Page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 const HomePage = () => {
   return (
-    <div className='text-3xl'>Home Page</div>
+    <div>
+      <h1 className='text-3xl'>Home Page</h1>
+      <Link href="/properties">Properties</Link>
+    </div> 
     )
 }
  
