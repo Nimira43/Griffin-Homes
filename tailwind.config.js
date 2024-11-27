7e44a39272a41163aa7736007a11221052868b9e
Nimira43 <n.rabone72@aol.com>
2024-11-27 20:06:07 +0000

    Full reboot - new project: Full stack Next JS app called Falcon Homes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-      },
-      gridTemplateColumns: {
-        '70/30': '70% 28%',
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-}
-
+};
