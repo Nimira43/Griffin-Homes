e3b0c51dc478ec2f8a87dae1802efe5664949cf1
Nimira43 <n.rabone72@aol.com>
2024-11-27 22:14:28 +0000

    Starting to style the navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <nav className='bg-[#008080] border-b-2 border-[#ff4500]'>
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-20 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center md:hidden"></div>
+
+        </div>
+      </div>
+    </nav>
+   )
+}
+ 
+export default Navbar
