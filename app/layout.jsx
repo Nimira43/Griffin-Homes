74ea09ad4a0825eb2a9213c901a1591d4c59cca9
Nimira43 <n.rabone72@aol.com>
2025-01-06 22:26:39 +0000

    Added project logo and metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,10 @@
 import '@/assets/styles/globals.css'
 
+export const metadata ={
+  title: 'Unicorn Homes',
+  keyword: 'Property website using Next JS'
+}
+
 const MainLayout = ({ children }) => {
   return ( 
     <html>
