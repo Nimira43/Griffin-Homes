1d1a97d912573eb0aa673c74d755437114bba2e5
Nimira43 <n.rabone72@aol.com>
2025-01-07 05:39:41 +0000

    Using useState to bring in Profile Menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,6 +7,7 @@ import user from '@/assets/images/user.png'
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
 
   return ( 
     <nav className='bg-prime-dark border-b border-dark'>
@@ -114,10 +115,11 @@ const Navbar = () => {
               <div>
                 <button
                   type='button'
-                  className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none'
+                  className='relative flex rounded-full bg-dark text-sm focus:outline-none'
                   id='user-menu-button'
                   aria-expanded='false'
                   aria-haspopup='true'
+                  onClick = { () => setIsProfileMenuOpen((prev) => !prev) }
                 >
                   <span className='absolute -inset-1.5'></span>
                   <span className='sr-only'>Open user menu</span>
@@ -128,41 +130,45 @@ const Navbar = () => {
                   />
                 </button>
               </div>
-              <div
-                id='user-menu'
-                className='hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-light py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
-                role='menu'
-                aria-orientation='vertical'
-                aria-labelledby='user-menu-button'
-                tabIndex='-1'
-              >
-                <Link
-                  href='/profile'
-                  className='block px-4 py-2 text-sm text-grey-dark'
-                  role='menuitem'
-                  tabIndex='-1'
-                  id='user-menu-item-0'
-                >
-                  Your Profile
-                </Link>
-                <Link
-                  href='/properties/saved'
-                  className='block px-4 py-2 text-sm text-grey-dark'
-                  role='menuitem'
-                  tabIndex='-1'
-                  id='user-menu-item-2'
-                >
-                  Saved Properties
-                </Link>
-                <button
-                  className='block px-4 py-2 text-sm text-grey-dark'
-                  role='menuitem'
-                  tabIndex='-1'
-                  id='user-menu-item-2'
-                >
-                  Sign Out
-                </button>
-              </div>
+              {
+                isProfileMenuOpen && ( 
+                  <div
+                    id='user-menu'
+                    className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-light py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                    role='menu'
+                    aria-orientation='vertical'
+                    aria-labelledby='user-menu-button'
+                    tabIndex='-1'
+                  >
+                    <Link
+                      href='/profile'
+                      className='block px-4 py-2 text-sm text-grey-dark hover:text-prime-medium'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-0'
+                    >
+                      Your Profile
+                    </Link>
+                    <Link
+                      href='/properties/saved'
+                      className='block px-4 py-2 text-sm text-grey-dark hover:text-prime-medium'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-2'
+                    >
+                      Saved Properties
+                    </Link>
+                    <button
+                      className='block px-4 py-2 text-sm text-grey-dark hover:text-prime-medium'
+                      role='menuitem'
+                      tabIndex='-1'
+                      id='user-menu-item-2'
+                    >
+                      Sign Out
+                    </button>
+                  </div>
+                )
+              } 
             </div>
           </div>
         </div>
@@ -202,5 +208,5 @@ const Navbar = () => {
     </nav>
   )
 }
- 
+
 export default Navbar
