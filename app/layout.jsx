5752c6fae766ea9f0ab1459b20f4f8a7f11f9710
Nimira43 <n.rabone72@aol.com>
2025-01-22 14:53:49 +0000

    Brought in Hero and InfoBoxes components into themain page.tsx and the Footer component into the main layout.tsx.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@/assets/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata ={
   title: 'Griffin Homes',
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
         <main>
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
