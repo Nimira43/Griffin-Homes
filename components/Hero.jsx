236e11c2381ded12e44a1df3ea644c480cdfc6f5
Nimira43 <n.rabone72@aol.com>
2025-01-22 15:35:57 +0000

    Hero messages.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,12 @@
 const Hero = () => {
   return ( 
     <section className="py-20 mb-4 bg-prime-light">
-
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-medium text-prime-dark-extra sm:text-5xl md:text-6xl logo">Explore & Discover</h1>
+          <p className="my-4 text-xl text-prime-dark-extra">Search our range of premium properties.</p>
+        </div>
+      </div>
     </section>
    )
 }
