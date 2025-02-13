5891be759d82325e14b8d7ee1185eb2247ffa571
Nimira43 <n.rabone72@aol.com>
2025-02-13 23:54:11 +0000

    Created Footer component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,35 @@
+import Image from 'next/image'
+import griffin from '@/assets/images/logo.png'
+
 const Footer = () => {
   return ( 
-    <div>Footer</div>
-   )
+    <footer className='bg-dark text-light py-4 mt-auto'>
+      <div
+        className='container mx-auto flex flex-col md:flex-row items-center justify-between px-4'
+      >
+        <div className='mb-4 md:mb-0'>
+          <Image
+            className='h-8 w-auto'
+            src={griffin}
+            alt='Griffin Homes'
+          />
+        </div>
+        <div
+          className='flex flex-wrap justify-center md:justify-start mb-4 md:mb-0'
+        >
+          <ul className='flex space-x-4'>
+            <li><a href='/properties.html'>Properties</a></li>
+            <li><a href='/terms.html'>Terms & Conditions</a></li>
+          </ul>
+        </div>
+        <div>
+          <p className='text-sm text-light mt-2 md:mt-0'>
+            &copy; 2025 Griffin Homes. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
 }
  
 export default Footer
