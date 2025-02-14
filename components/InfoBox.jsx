fce120688e6c4a80cdd993290db0413045dc49c7
Nimira43 <n.rabone72@aol.com>
2025-02-14 22:48:27 +0000

    Starting to refactor InfoBoxes

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,7 +1,23 @@
-const InfoBox = () => {
+import Link from 'next/link'
+
+const InfoBox = ({
+  heading,
+  
+  children
+
+
+}) => {
+
   return ( 
-    <div>
-      Info Box
+    <div className='bg-prime-info-1 p-6 rounded-lg shadow-md'>
+      <h2 className='text-2xl text-prime-dark-extra font-semibold'>{ heading }</h2>
+      <p className='mt-2 mb-4'>{children}</p>
+      <Link
+        href='/properties'
+        className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-prime-dark-extra hover:text-grey-light'
+      >
+        Check Properties
+      </Link>
     </div>
    )
 }
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,26 +1,42 @@
-import Link from 'next/link'
+// import Link from 'next/link'
+import InfoBox from './InfoBox'
 
 const InfoBoxes = () => {
   return ( 
     <section>
       <div className='container-xl lg:container m-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
-          <div className='bg-prime-light p-6 rounded-lg shadow-md'>
-            <h2 className='text-2xl text-prime-dark-extra font-semibold'>For Renters</h2>
-            <p className='mt-2 mb-4'>
-              Discover your ideal rental property. Save your favourite listings and get in touch with the owners.
-            </p>
-            <Link
-              href='/properties'
-              className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-prime-dark-extra hover:text-grey-light'
-            >
-              Check Properties
-            </Link>
-          </div>
-          <div className='bg-prime-light p-6 rounded-lg shadow-md'>
+
+
+
+
+          <InfoBox
+            heading='For Renters'
+            
+          >Discover your ideal rental property. Save your favourite listings and get in touch with the owners.</InfoBox>
+
+
+
+
+
+
+          <InfoBox
+            heading='For Property Owners'
+          
+          >Showcase your properties and attract potential tenants. Rent out your space as a holiday let or opt for long-term leasing.</InfoBox>
+          
+          
+          
+          
+          
+          
+                 
+          
+          
+          {/* <div className='bg-prime-info-2 p-6 rounded-lg shadow-md'>
             <h2 className='text-2xl text-prime-dark-extra font-semibold'>For Property Owners</h2>
             <p className='mt-2 mb-4'>
-              Showcase your properties and attract potential tenants. Rent out your space as a holiday let or opt for long-term leasing..
+              Showcase your properties and attract potential tenants. Rent out your space as a holiday let or opt for long-term leasing.
             </p>
             <Link
               href='/add-property'
@@ -28,7 +44,13 @@ const InfoBoxes = () => {
             >
               Add Property
             </Link>
-          </div>
+          </div> */}
+        
+        
+        
+        
+        
+        
         </div>
       </div>
     </section>
