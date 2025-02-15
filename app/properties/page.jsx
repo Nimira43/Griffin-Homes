2ebff83ce00d1e14bfb8cdabf7e66d23a42445dc
Nimira43 <n.rabone72@aol.com>
2025-02-15 02:00:00 +0000

    Passing in key for PropertyCard.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -9,8 +9,10 @@ const PropertiesPage = () => {
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {
               properties.map((property) => (
-                // <div>{property.name}</div>
-                <PropertyCard />
+                <PropertyCard
+                  key={property._id}
+                  property={property}
+                />
               ))
             }
           </div>
