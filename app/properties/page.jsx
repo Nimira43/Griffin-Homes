1dd7e5d3a138309c0c350b3c4103dce1ac5218d1
Nimira43 <n.rabone72@aol.com>
2025-02-15 01:05:30 +0000

    Brought PropertyCard into properties/page.tsx

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,3 +1,4 @@
+import PropertyCard from '@/components/PropertyCard'
 import properties from '@/properties.json'
 
 const PropertiesPage = () => {
@@ -8,7 +9,8 @@ const PropertiesPage = () => {
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {
               properties.map((property) => (
-                <div>{property.name}</div>
+                // <div>{property.name}</div>
+                <PropertyCard />
               ))
             }
           </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,69 +1,69 @@
 const Hero = () => {
   return ( 
     <section 
-      className="py-20 mb-4 bg-prime-light"
+      className='py-20 mb-4 bg-prime-light'
     >
       <div 
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+        className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'
       >
         <div 
-          className="text-center"
+          className='text-center'
         >
           <h1 
-            className="text-4xl font-medium text-prime-dark-extra sm:text-5xl md:text-6xl logo"
+            className='text-4xl font-medium text-prime-dark-extra sm:text-5xl md:text-6xl logo'
           >
             Explore & Discover
           </h1>
           <p 
-            className="my-4 text-xl text-prime-dark-extra"
+            className='my-4 text-xl text-prime-dark-extra'
           >
             Search our range of premium properties.
           </p>
         </div>
         <form 
-          className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
+          className='mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center'
         >
           <div 
-            className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0"
+            className='w-full md:w-3/5 md:pr-2 mb-4 md:mb-0'
           >
             <label 
-              htmlFor="location" 
-              className="sr-only"
+              htmlFor='location' 
+              className='sr-only'
             >
               Location
             </label>
             <input
               type='text'
               id='location'
-              placeholder="Enter location"
-              className="w-full px-4 py-3 rounded-md bg-light text-dark focus:outline-none focus:ring-1 focus:ring-prime-dark-extra" 
+              placeholder='Enter location'
+              className='w-full px-4 py-3 rounded-md bg-light text-dark focus:outline-none focus:ring-1 focus:ring-prime-dark-extra' 
             />
           </div>
           <div 
-            className="w-full md:w-2/5 md:pl-2"
+            className='w-full md:w-2/5 md:pl-2'
           >
             <label 
-              htmlFor="property-type"
-              className="sr-only"
+              htmlFor='property-type'
+              className='sr-only'
             >
               Property Type
             </label>
             <select
-              id="property-type" 
-              className="w-full px-4 py-3 rounded-md bg-light text-dark focus:outline-none focus:ring-1 focus:ring-prime-dark-extra"
+              id='property-type' 
+              className='w-full px-4 py-3 rounded-md bg-light text-dark focus:outline-none focus:ring-1 focus:ring-prime-dark-extra'
             >
-              <option value="All">All</option>
-              <option value="Apartment">Apartment</option>
-              <option value="Studio">Studio</option>
-              <option value="Flat">Flat</option>
-              <option value="House">House</option>
-              <option value="Cabin Or Cottage">Cabin Or Cottage</option>
-              <option value="Loft">Loft</option>
-              <option value="Room">Room</option>
-              <option value="Other">Other</option>
+              <option value='All'>All</option>
+              <option value='Apartment'>Apartment</option>
+              <option value='Studio'>Studio</option>
+              <option value='Flat'>Flat</option>
+              <option value='House'>House</option>
+              <option value='Cabin Or Cottage'>Cabin Or Cottage</option>
+              <option value='Loft'>Loft</option>
+              <option value='Room'>Room</option>
+              <option value='Other'>Other</option>
             </select>
           </div>
-          <button className="md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 rounded-md bg-dark text-light hover:bg-prime-dark-extra hover:text-grey-light focus:outline-none uppercase "
+          <button className='md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 rounded-md bg-dark text-light hover:bg-prime-dark-extra hover:text-grey-light focus:outline-none uppercase '
           >
             Search
           </button>
