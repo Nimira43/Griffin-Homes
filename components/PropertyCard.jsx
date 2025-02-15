d75a78fff04e0ead7f6adb4c52a3a3e158dced38
Nimira43 <n.rabone72@aol.com>
2025-02-15 00:58:06 +0000

    Setting up imports

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,4 +1,10 @@
-const PropertyCard = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+import { LiaBedSolid, LiaBathSolid, LiaRulerCombinedSolid} from 'react-icons/lia'
+import { RiMoneyPoundCircleLine } from 'react-icons/ri'
+import { MdOutlineLocationOn } from "react-icons/md";
+
+const PropertyCard = ({ property }) => {
   return ( 
 
    )
