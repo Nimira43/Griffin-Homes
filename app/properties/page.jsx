9c17a374539b0275f4a3ef8d5fb5221118800a29
Nimira43 <n.rabone72@aol.com>
2025-02-15 00:43:01 +0000

    Starting work on the Properties page.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,14 @@
+import properties from '@/properties.json'
+
 const PropertiesPage = () => {
   return ( 
-    <div>Properties Page</div>
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {properties.length === 0 ? (<p>No Properties Found</p>) : (
+          <p>Properites</p>
+        ) }
+      </div>
+    </section>
    )
 }
  
