5695b648866756a96ce9eec168add98eba9cb24e
Nimira43 <n.rabone72@aol.com>
2025-02-15 01:35:11 +0000

    Brought in Proerty assets. Updated the PropertyCard to correctly bring in the image. Now using the Link tag. Set up new icons.

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -3,14 +3,15 @@ import Link from 'next/link'
 import { LiaBedSolid, LiaBathSolid, LiaRulerCombinedSolid} from 'react-icons/lia'
 import { RiMoneyPoundCircleLine } from 'react-icons/ri'
 import { MdOutlineLocationOn } from 'react-icons/md';
+import rental from '@/assets/images/properties/a1.jpg'
 
 const PropertyCard = ({ property }) => {
   
   return ( 
     <div className='rounded-xl shadow-md relative'>
-      <img
-        src='images/properties/a1.jpg'
-        alt=''
+      <Image
+        src={rental}
+        alt='Rental'
         className='w-full h-auto rounded-t-xl'
       />
       <div className='p-4'>
@@ -26,15 +27,15 @@ const PropertyCard = ({ property }) => {
 
         <div className='flex justify-center gap-4 text-grey-dark mb-4'>
           <p>
-            <i className='fa-solid fa-bed'></i> 3
+            <LiaBedSolid className='fa-solid fa-bed' /> 3
             <span className='md:hidden lg:inline'> Beds</span>
           </p>
           <p>
-            <i className='fa-solid fa-bath'></i> 2
+            <LiaBathSolid className='fa-solid fa-bath' /> 2
             <span className='md:hidden lg:inline'> Baths</span>
           </p>
           <p>
-            <i className='fa-solid fa-ruler-combined'></i> 1,500
+            <LiaRulerCombinedSolid className='fa-solid fa-ruler-combined' /> 1,500
             <span className='md:hidden lg:inline'>sqft</span>
           </p>
         </div>
@@ -42,25 +43,25 @@ const PropertyCard = ({ property }) => {
         <div
           className='flex justify-center gap-4 text-prime-medium-extra text-sm mb-4'
         >
-          <p><i className='fa-solid fa-money-bill'></i> Weekly</p>
-          <p><i className='fa-solid fa-money-bill'></i> Monthly</p>
+          <p><RiMoneyPoundCircleLine className='fa-solid fa-money-bill' /> Weekly</p>
+          <p><RiMoneyPoundCircleLine className='fa-solid fa-money-bill' /> Monthly</p>
         </div>
 
         <div className='border border-prime-light mb-5'></div>
 
         <div className='flex flex-col lg:flex-row justify-between mb-4'>
           <div className='flex align-middle gap-2 mb-4 lg:mb-0'>
-            <i
+            <MdOutlineLocationOn
               className='fa-solid fa-location-dot text-lg text-prime-medium-extra'
-            ></i>
+            />
             <span className='text-prime-medium-extra'> Boston MA </span>
           </div>
-          <a
+          <Link
             href='property.html'
             className='h-[36px] bg-dark hover:bg-prime-dark-extra text-light hover:text-grey-light uppercase px-4 py-2 rounded-lg text-center text-sm'
           >
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
