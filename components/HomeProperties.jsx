e9854e8d5aabb0c8f6349d51ac717d41f01bb88f
Nimira43 <n.rabone72@aol.com>
2025-02-18 19:51:47 +0000

    Added h2 title to the HomeProperties component.

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -5,6 +5,7 @@ const HomeProperties = () => {
   return ( 
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
+        <h2 className='text-3xl font-semibold text-prime-dark-extra mb-6 text-center'>Latest Properties</h2>
         {properties.length === 0 ? (<p>No Properties Found</p>) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {
