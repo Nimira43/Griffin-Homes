f9d7782ff0c330596dab121f2b9851c54988f35c
Nimira43 <n.rabone72@aol.com>
2025-02-28 01:52:23 +0000

    Created and used variable to limit the amount of properties displayed on home page.

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -2,6 +2,8 @@ import properties from '@/properties.json'
 import PropertyCard from './PropertyCard'
 
 const HomeProperties = () => {
+  const recentProperties = properties.slice(0, 3)
+
   return ( 
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
@@ -9,7 +11,7 @@ const HomeProperties = () => {
         {properties.length === 0 ? (<p>No Properties Found</p>) : (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
             {
-              properties.map((property) => (
+              recentProperties.map((property) => (
                 <PropertyCard
                   key={property._id}
                   property={property}
