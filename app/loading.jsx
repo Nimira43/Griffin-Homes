a48cf9533d8bac0db0c23a9944eb39740c6286ec
Nimira43 <n.rabone72@aol.com>
2025-03-17 16:45:21 +0000

    Configured the ClipLoader spinner.

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,10 +2,20 @@
 
 import ClipLoader from 'react-spinners/ClipLoader'
 
+const override = {
+  display: 'block',
+  margin: '100px auto'
+}
+
 const LoadingPage = () => {
   return ( 
-    <div>Loading...</div>
-   )
+    <ClipLoader
+      color='#036262'
+      cssOverride={override}
+      size={150}
+      aria-label='Loading Spinner'
+    />
+  )
 }
  
 export default LoadingPage
